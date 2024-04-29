a9b001b49e3d7683718e6b10dd6cf8e12a2ca45e
thule129 <baekhyunee129@gmail.com>
2024-04-29 20:12:02 +0700

    second commit

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Test extends FormRequest
+class StoreItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
